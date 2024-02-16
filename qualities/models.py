2d027d69f3e3afe5efdb8fcebb97e8b321d3988a
Vipul Kushwaha <vipulkushwaha@gmail.com>
2024-02-16 23:17:33 +0530

    Add 'Hobby' model and register in admin site

--- qualities/models.py
+++ qualities/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
+from django.utils.text import slugify
 
 # Create your models here.
+class Hobby(models.Model):
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(verbose_name='name', max_length=100, unique=True)
+    slug = models.SlugField(verbose_name='slug', null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        value = f'{self.name}'
+        self.slug = slugify(value, allow_unicode=True)
+        super().save(*args, **kwargs)
