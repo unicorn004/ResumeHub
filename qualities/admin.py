93a810aae71b2f8ecc25074320af31af3d2331a9
Amogh Shetty <amoghvjshetty@gmail.com>
2024-02-19 18:54:26 +0000

    Update 2 files
    - /qualities/models.py
    - /qualities/admin.py

--- qualities/admin.py
+++ qualities/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import Hobby
+from .models import Hobby, Testimonial, WorkExperience, Resume
 
-# Register your models here.
 @admin.register(Hobby)
 class HobbyAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'slug', 'unique_id')
 
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('source', 'content', 'date', 'unique_id')
+
+@admin.register(WorkExperience)
+class WorkExperienceAdmin(admin.ModelAdmin):
+    list_display = ('position', 'company', 'location', 'employment_duration', 'unique_id' ,'responsibilities')
+
+@admin.register(Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ('unique_id','personal_information','education','skills','work_experience','achievements','projects','additional_sections')
--- qualities/models.py
+++ qualities/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 from django.utils.text import slugify
 
-# Create your models here.
+# Hobby
 class Hobby(models.Model):
     unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(verbose_name='name', max_length=100, unique=True)
@@ -12,3 +12,46 @@ class Hobby(models.Model):
         value = f'{self.name}'
         self.slug = slugify(value, allow_unicode=True)
         super().save(*args, **kwargs)
+
+
+# Testimonial
+class Testimonial(models.Model):
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    source = models.CharField(verbose_name='Source', max_length=255)
+    content = models.TextField(verbose_name='Content')
+    date = models.DateField(verbose_name='Date')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+
+# Work Experience
+class WorkExperience(models.Model):
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    position = models.CharField(verbose_name='Position', max_length=255)
+    company = models.CharField(verbose_name='Company/Organization', max_length=255)
+    location = models.CharField(verbose_name='Location', max_length=255)
+    employment_duration = models.CharField(verbose_name='Employment Duration', max_length=255)
+    responsibilities = models.TextField(verbose_name='Responsibilities')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+
+# Resume
+class Resume(models.Model):
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    personal_information = models.TextField(verbose_name='Personal Information')
+    education = models.TextField(verbose_name='Education')
+    skills = models.TextField(verbose_name='Skills')
+    work_experience = models.TextField(verbose_name='Work Experience')
+    achievements = models.TextField(verbose_name='Achievements')
+    projects = models.TextField(verbose_name='Projects')
+    additional_sections = models.TextField(verbose_name='Additional Sections', blank=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+
+
+
