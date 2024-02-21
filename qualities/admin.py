1e1d30b30ae7539e4b3c6b4a660ae84814a50b07
Amogh Shetty <amoghvjshetty@gmail.com>
2024-02-21 18:24:28 +0000

    Update 2 files
    - /qualities/admin.py
    - /qualities/models.py

--- qualities/admin.py
+++ qualities/admin.py
@@ -7,12 +7,20 @@ class HobbyAdmin(admin.ModelAdmin):
 
 @admin.register(Testimonial)
 class TestimonialAdmin(admin.ModelAdmin):
-    list_display = ('source', 'content', 'date', 'unique_id')
+    list_display = ('source', 'content', 'date', 'unique_id' ,'candidate')
 
 @admin.register(WorkExperience)
 class WorkExperienceAdmin(admin.ModelAdmin):
-    list_display = ('position', 'company', 'location', 'employment_duration', 'unique_id' ,'responsibilities')
+    list_display = ('candidate','designation', 'company', 'location', 'employment_duration', 'unique_id' ,'responsibilities')
 
 @admin.register(Resume)
 class ResumeAdmin(admin.ModelAdmin):
     list_display = ('unique_id','personal_information','education','skills','work_experience','achievements','projects','additional_sections')
+
+@admin.register(Achievement)
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ('unique_id','candidate','name','description','date_achieved')
+
+@admin.register(Project)
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ('unique_id','candidate','name','description','start_date','end_date')
--- qualities/models.py
+++ qualities/models.py
@@ -8,25 +8,22 @@ class Hobby(models.Model):
     name = models.CharField(verbose_name='name', max_length=100, unique=True)
     slug = models.SlugField(verbose_name='slug', null=True, blank=True)
 
-    def save(self, *args, **kwargs):
-        value = f'{self.name}'
-        self.slug = slugify(value, allow_unicode=True)
-        super().save(*args, **kwargs)
 
 
 # Testimonial
 class Testimonial(models.Model):
     unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    candidate = models.ForeignKey(User, verbose_name='Candidate', on_delete=models.CASCADE, blank=True, null=True)
     source = models.CharField(verbose_name='Source', max_length=255)
     content = models.TextField(verbose_name='Content')
     date = models.DateField(verbose_name='Date')
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+
 
 
 # Work Experience
 class WorkExperience(models.Model):
+    candidate = models.ForeignKey(User, verbose_name='Candidate', on_delete=models.CASCADE, blank=True, null=True)
     unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     position = models.CharField(verbose_name='Position', max_length=255)
     company = models.CharField(verbose_name='Company/Organization', max_length=255)
@@ -34,23 +31,41 @@ class WorkExperience(models.Model):
     employment_duration = models.CharField(verbose_name='Employment Duration', max_length=255)
     responsibilities = models.TextField(verbose_name='Responsibilities')
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
 
 
-# Resume
+# Project
+class Project(models.Model):
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    candidate = models.ForeignKey(User, verbose_name='Candidate', on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(verbose_name='Project Name', max_length=255)
+    description = models.TextField(verbose_name='Description', blank=True)
+    start_date = models.DateField(verbose_name='Start Date', blank=True, null=True)
+    end_date = models.DateField(verbose_name='End Date', blank=True, null=True)
+
+
+# Achievement
+class Achievement(models.Model):
+    name = models.CharField(verbose_name='Achievement Name', max_length=255)
+    candidate = models.ForeignKey(User, verbose_name='Candidate', on_delete=models.CASCADE, blank=True, null=True)
+    description = models.TextField(verbose_name='Description', blank=True)
+    date_achieved = models.DateField(verbose_name='Date Achieved', blank=True, null=True)
+    related_project = models.ForeignKey(Project, verbose_name='Related Project', on_delete=models.CASCADE, blank=True, null=True)
+    certificate_file = models.FileField(verbose_name='Certificate File', upload_to='certificates/', blank=True, null=True)
+
+
+# Resume 
 class Resume(models.Model):
     unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     personal_information = models.TextField(verbose_name='Personal Information')
     education = models.TextField(verbose_name='Education')
     skills = models.TextField(verbose_name='Skills')
-    work_experience = models.TextField(verbose_name='Work Experience')
-    achievements = models.TextField(verbose_name='Achievements')
-    projects = models.TextField(verbose_name='Projects')
+    achievements = models.ManyToManyField(Achievement, verbose_name='Achievements')
+    projects = models.ManyToManyField(Project, verbose_name='Projects')
     additional_sections = models.TextField(verbose_name='Additional Sections', blank=True)
+    work_experience = models.ForeignKey(WorkExperience, verbose_name='Work Experience', on_delete=models.CASCADE)
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+
+
 
 
 
