5cfb33a638a1ec183f4e6debba89d4103025d577
amoghvoleti <amogh.voleti22@spit.ac.in>
2024-02-25 13:12:43 +0530

    Add 'Hobby' model and register in admin site

--- qualities/admin.py
+++ qualities/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Hobby
 
 # Register your models here.
+@admin.register(Hobby)
+class HobbyAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
