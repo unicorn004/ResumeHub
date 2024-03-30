56f0bccb01090b8d996420ac08184eb884643162
Tanmay Sinkar <tanmaysinkar2021@gmail.com>
2024-03-30 21:48:09 +0530

    added view for login page

--- resumehub/views.py
+++ resumehub/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
 
 def homepage(request):
     return render(request, 'homepage/index.html')
+
+def login_page(request):
+    form = AuthenticationForm()
+    return render(request, 'user_login/login.html', {'form': form})
