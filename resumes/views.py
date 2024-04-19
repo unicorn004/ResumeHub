e56dad2c3e180191682934ff6980c7be92992a90
Vipul Kushwaha <vipulkushwaha@gmail.com>
2024-04-19 10:34:46 +0530

    Add resume form

--- resumes/views.py
+++ resumes/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from resumes.models import Resume
+from .forms import ResumeForm
 
 # Create your views here.
 
 @login_required(login_url = 'accounts:login')
 def resumes(request):
+    if 'create' in request.GET:
+        form = ResumeForm()
+        context = {
+            'user': request.user,
+            'form': form
+        }
+        return render(request, 'resumes/create_resume.html', context)
+    
     resumes = Resume.objects.filter(candidate=request.user)
     context = {
         'user':request.user,
