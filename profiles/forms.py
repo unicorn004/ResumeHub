7dd8fdf82ca559cb69848cf78b6f9c4f2f61aede
amoghvoleti <amogh.voleti22@spit.ac.in>
2024-04-24 00:45:24 +0530

    profile form submit

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from django.core.validators import MinValueValidator, MaxValueValidator
+from .models import Profile
 
-class UserProfileForm(forms.Form):
+class UserProfileForm(forms.ModelForm):
     full_name = forms.CharField(label='Full Name', max_length=100)
     address = forms.CharField(label='Address', widget=forms.Textarea)
     age = forms.IntegerField(label='Age', validators=[MinValueValidator(0), MaxValueValidator(150)])
     phone_number = forms.CharField(label='Phone Number', max_length=15)
     email = forms.EmailField(label='Email Address')
+
+    class Meta:
+        model = Profile
+        fields = ['full_name', 'address', 'age', 'phone_number', 'email']  # Rearrange fields
