9cad9abe398ec2fb195ca86622a3f257988ac98b
amoghvoleti <amogh.voleti22@spit.ac.in>
2024-04-24 20:10:19 +0530

    form update

--- qualities/forms.py
+++ qualities/forms.py
@@ -19,9 +19,12 @@ from .models import Education
 
 class EducationForm(forms.ModelForm):
     full_name = forms.CharField(label='Full Name', max_length=100)
+    education_level = forms.ChoiceField(label='Education Level', choices=Education.EDUCATION_LEVEL_CHOICES)
     date_of_passing = forms.DateField(label='Date of Passing', widget=forms.TextInput(attrs={'class': 'datepicker'}))
     additional_info = forms.CharField(label='Additional Info', widget=forms.Textarea)
     pursuing = forms.BooleanField(label='Pursuing', required=False)  # Add this line
+    
+
 
     class Meta:
         model = Education
